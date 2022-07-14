ec01c7a2d0ba35d33d721d3ff1ef2bf63e756f7c
HassanMah1 <hassanmahmoodpakk@gmail.com>
2022-07-14 19:35:55 +0500

    loops2

---
+++ loops.py
@@ -0,0 +1,19 @@
+import pymongo
+print("-----------WHILE LOOPS----------------")
+
+i=0
+while i<5:
+    print(i)
+    i=i+1
+
+print("------------For Loop------------")
+for item in range(6):
+    print(item)
+
+print("---------NESTED FOR LOOP--------")
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
