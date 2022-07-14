bba260e0bdf4414a0773b55655c53a6e59085a81
HassanMah1 <hassanmahmoodpakk@gmail.com>
2022-07-14 23:16:35 +0500

    mongodb create and insert

--- loops.py
+++ loops.py
@@ -10,7 +10,7 @@ print("------------For Loop------------")
 for item in range(6):
     print(item)
 
-print("---------NESTED FOR LOOP--------")
+print("---------NESTED FOR LOOP---------")
 adj = ["red", "big", "tasty"]
 fruits = ["apple", "banana", "cherry"]
 
